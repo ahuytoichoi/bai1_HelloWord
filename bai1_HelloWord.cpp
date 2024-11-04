40cc7edcb697d7d8258b7a6b8f20c30afa624038
ahuytoichoi <MAY 06@LAB2-31>
2024-11-04 11:24:16 +0700

    Add project files.

---
+++ bai1_HelloWord.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "hello worlds!\n";
+	cout << "toitenla" << "luonghoanghuy" << endl;
+	cout << "toi dang hoc c++" << endl;
+	return 0;
+}
